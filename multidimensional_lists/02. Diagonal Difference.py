e3e8dbee097cd6c38bbf618ed35efcf87453c4f2
hmustafa95 <116489515+hmustafa95@users.noreply.github.com>
2022-10-23 13:37:13 +0300

    Add files via upload

---
+++ multidimensional_lists/02. Diagonal Difference.py
@@ -0,0 +1,14 @@
+size = int(input())
+matrix = []
+
+for _ in range(size):
+    matrix.append([int(x) for x in input().split()])
+
+primary = 0
+secondary = 0
+for idx in range(size):
+    primary += matrix[idx][idx]
+    secondary += matrix[idx][size - idx - 1]
+
+result = abs(primary - secondary)
+print(result)
